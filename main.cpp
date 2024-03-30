f28d1d4da4890b228197b7669752532c1cc32c51
Natalie Petersen <npetersen1380@sdsu.edu>
2024-03-29 18:16:10 -0700

    3/29/24 6:15pm - finished! too many changes to note but basically finished delete method and stuff

--- main.cpp
+++ main.cpp
@@ -29,21 +29,16 @@ private:
     Node<T> *rightChild;
     Node<T> *parent;
 public:
-    explicit Node(T *data) {
+    Node(T *data) {
         this->data = data;
         this->leftChild = nullptr;
         this->rightChild = nullptr;
         this->parent = nullptr;
     }
 
-    void print() { //this works, not sure how so don't touch it for now
-        data->print();
+    void print() { data->print(); }
 
-    }
-
-    T *getData() {
-        return data;
-    }
+    T *getData() { return data; }
 
     Node<T> *getLeftChild() { return leftChild; }
 
@@ -56,6 +51,32 @@ public:
     void setRightChild(Node<T> *c) { rightChild = c; }
 
     void setParent(Node<T> *c) { parent = c; }
+
+    int compareToNode(Node<T> *other) {
+        if (this->getData()->getValue() < other->getData()->getValue()) {//this is less than other
+            return -1;
+        } else if (this->getData()->getValue() == other->getData()->getValue()) { //this is equal to other
+            return 0;
+        } else //this is less than other
+            return 1;
+    }
+
+    int compareToData(T *other) {
+        if (this->getData()->getValue() < other->getValue()) {//this is less than other
+            return -1;
+        } else if (this->getData()->getValue() == other->getValue()) { //this is equal to other
+            return 0;
+        } else //this is greater than other
+            return 1;
+    }
+
+    bool deleteNode() {
+        setParent(nullptr);
+        setLeftChild(nullptr);
+        setRightChild(nullptr);
+        return true;
+    }
+
 };
 
 template<typename T>
@@ -77,56 +98,17 @@ public:
     ~BinarySearchTree() {
         while (numberOfElements > 0)
             deleteElement(root);
-
     }
 
-    //write setters and getters
-    //elem should be a Data element, in this case (whatever the Node Class is using as its datatype)
     void insertElement(T *d) {
         Node<T> *elem = new Node<T>(d);
         if (numberOfElements == 0) //accounting for if the entire tree is deleted for some reason
             root = elem;
         else {
-            elem->setParent(findParent(elem));
+            elem->setParent(findParent(elem)); //setting parent so we know where to insert beforehand
             insertNonRoot(elem, elem->getParent());
         }
-        numberOfElements++;
-    }
-
-    bool insertNonRoot(Node<T> *elem, Node<T> *parent) {
-        if (elem->getData()->getValue() <=
-            parent->getData()->getValue()) { //if element's value is less than parent's value, insert left
-            parent->setLeftChild(elem); //setting elem to be the child of its parent
-            return true; //inserted left child
-        } else if (elem->getData()->getValue() >
-                   parent->getData()->getValue()) { //if element's value is greater, insert right
-            parent->setRightChild(elem); //setting elem to be the child of its parent
-            return true; //inserted right child
-        }
-        return false; //insertion failed
-
-    }
-
-    Node<T> *findParent(Node<T> *elem) { //could make recursive?
-        Node<T> *curr = root;
-        int ctr = 0;
-        while (ctr < numberOfElements) { //prevents an infinite loop for now
-            if (elem->getData()->getValue() <=
-                curr->getData()->getValue()) { //if value of new element is less than or = to parent, go to the left
-                if (curr->getLeftChild() == nullptr)
-                    return curr;
-                else
-                    curr = curr->getLeftChild();
-            } else if (elem->getData()->getValue() >
-                       curr->getData()->getValue()) { //if greater than parent, go to the right
-                if (curr->getRightChild() == nullptr)
-                    return curr;
-                else
-                    curr = curr->getRightChild();
-            }
-            ctr++;
-        }
-        return root;
+        numberOfElements++; //updating number of elements
     }
 
     void print() {
@@ -134,19 +116,7 @@ public:
         cout << endl;
     }
 
-    void printRecursive(Node<T> *curr) {
-        if (curr == nullptr)
-            return; //base case, if there isn't anything there don't bother trying to print
-        else {
-            curr->print(); //print root
-            cout << " ";
-            printRecursive(curr->getLeftChild()); //print left subtree
-            printRecursive(curr->getRightChild()); //print right subtree
-        }
-    }
-
     void findSmallest() {
-        cout << "smallest number is: ";
         sortedList.clear(); //clears list to make room for the new elements - could change the way sorting works to eliminate this step
         sortRecursive(root, true); //sorting list in ascending order
         sortedList.front()->print(); //prints top item (smallest since ascending)
@@ -154,29 +124,78 @@ public:
     }
 
     void findBiggest() {
-        cout << "biggest number is: ";
         sortedList.clear(); //clears list
         sortRecursive(root, false); //sorting list in descending order
         sortedList.front()->print(); //prints top item (largest since descending)
         cout << endl;
     }
 
-    bool deleteElement(T *data) {
-        cout << "delete element" << endl; //best method here
-        return false;
-    }
-
-    void findKthElement(int k) {
-        if (k >= numberOfElements) { //trying to print an element at an index that doesn't exist/out of bounds
+    void deleteElement(T *data) {
+        Node<T> *elem = findNode(data, root);
+        if (elem == nullptr) {
             cout << "Number does NOT exist" << endl;
             return;
         }
-        cout << k << "th element is: ";
+        Node<T> *elemChild = nullptr;
+        //deleting a leaf node (no children)
+        if (elem->getRightChild() == nullptr && elem->getLeftChild() == nullptr) {
+            if (elem->compareToNode(elem->getParent()) < 0) //if elem is less than its parent (lc)
+                elem->getParent()->setLeftChild(nullptr);
+            else
+                elem->getParent()->setRightChild(nullptr);
+            elem->deleteNode(); //deleting child node
+            numberOfElements--;
+        }
+            //deleting an internal node with one child (but not both)
+        else if (!(elem->getLeftChild() && elem->getRightChild())) {
+            //left child only
+            if (elem->getLeftChild() != nullptr) {
+                elemChild = elem->getLeftChild();
+                elemChild->setParent(elem->getParent()); //updating parent relation
+                if (elem->getParent() != nullptr)
+                    elem->getParent()->setLeftChild(elemChild); //updating predecessor's child relation
+                else
+                    root = elemChild;
+            }
+                //right child but no left child
+            else {
+                elemChild = elem->getRightChild();
+                elemChild->setParent(elem->getParent()); //same process as before
+                if (elem->getParent() != nullptr)
+                    elem->getParent()->setRightChild(elemChild);
+                else
+                    root = elemChild;
+            }
+            //regardless of case, delete the node we need to delete and decrease # of elements in tree
+            elem->deleteNode();
+            numberOfElements--;
+        }
+            //two children scenario
+        else if (elem->getLeftChild() != nullptr && elem->getRightChild() != nullptr) {
+            Node<T> *suc = findSuccessor(elem);
+            suc->setRightChild(elem->getRightChild());
+            suc->getRightChild()->setParent(suc); //fixing the child of successor's info for parent
+            suc->setLeftChild(
+                    elem->getLeftChild()); //setting successor's left child to the left subtree of deleted node
+            suc->getLeftChild()->setParent(suc); //making this relationship go both ways
+            suc->getParent()->setRightChild(nullptr); //removing successor from parent's child list
+            suc->setParent(elem->getParent());
+            root = suc;
+            //deleting node and fixing element #
+            elem->deleteNode();
+            numberOfElements--;
+        }
+    }
+
+    void findKthElement(int k) {
+        if (k > numberOfElements) { //trying to print an element at an index that doesn't exist/out of bounds
+            cout << "Number does NOT exist" << endl;
+            return;
+        }
         sortedList.clear(); //clears list to make room for new elements
         sortRecursive(root, true); //sorting list in ascending order
-        sortedList.at(k)->print(); //prints kth element
+        sortedList.at(k - 1)->print(); //prints kth element
         cout << endl;
-
     }
 
     int getnumberOfElements() { return this->numberOfElements; }
@@ -188,6 +207,23 @@ public:
         sortRecursive(root, true);
         //prints elements
         printList();
+    }
+
+    void sortDescending() {
+        sortRecursive(root,
+                      false); //still sorting recursively, but the false bool means that the list will be backwards
+        printList();
+    }
+
+
+private:
+    void printList() {
+        sortedList.front()->getData()->print(); //prints first element without comma
+        for (int i = 1; i < numberOfElements; i++) {
+            cout << ", ";
+            sortedList.at(i)->getData()->print();
+        }
+        cout << endl;
     }
 
     void sortRecursive(Node<T> *curr, bool inc) {
@@ -204,20 +240,73 @@ public:
         }
     }
 
-    void sortDescending() {
-        sortRecursive(root,false); //still sorting recursively, but the false bool means that the list will be backwards
-        printList();
+    Node<T> *findSuccessor(Node<T> *elem) {
+        sortedList.clear();
+        sortRecursive(elem->getLeftChild(),
+                      false); //gets left subtree in descending order so that rightmost is in front
+        return sortedList.front();
     }
 
-    void printList() {
-        sortedList.front()->getData()->print(); //prints first element without comma
-        for (int i = 1; i < numberOfElements; i++) {
-            cout << ", ";
-            sortedList.at(i)->getData()->print();
+    Node<T> *findNode(T *data, Node<T> *curr) {
+        if (curr->compareToData(data) == 0) //if curr's value is what we're looking for, return
+            return curr;
+        else if (curr->compareToData(data) > 0) { //if curr is greater than what we're looking for, go left
+            if (curr->getLeftChild() == nullptr)
+                return nullptr;
+            return findNode(data, curr->getLeftChild());
+        } else if (curr->compareToData(data) < 0) { //if curr is less than what we're looking for, go right
+            if (curr->getRightChild() == nullptr)
+                return nullptr;
+            return findNode(data, curr->getRightChild());
         }
-        cout << endl;
+        return nullptr;
     }
 
+    void printRecursive(Node<T> *curr) {
+        if (curr == nullptr)
+            return; //base case, if there isn't anything there don't bother trying to print
+        else {
+            if(curr!=root)
+                cout << ", ";
+            curr->print(); //print root
+            printRecursive(curr->getLeftChild()); //print left subtree
+            printRecursive(curr->getRightChild()); //print right subtree
+        }
+    }
+
+    Node<T> *findParent(Node<T> *elem) { //could make recursive?
+        Node<T> *curr = root;
+        while (true) {
+            //if value of new element is less than or == to parent, go to left subtree
+            if (elem->getData()->getValue() <= curr->getData()->getValue()) {
+                if (curr->getLeftChild() == nullptr)
+                    return curr;
+                else
+                    curr = curr->getLeftChild();
+            }
+                //if greater than parent, go to the right subtree
+            else if (elem->getData()->getValue() > curr->getData()->getValue()) {
+                if (curr->getRightChild() == nullptr)
+                    return curr;
+                else
+                    curr = curr->getRightChild();
+            }
+        }
+        return root;
+    }
+
+    bool insertNonRoot(Node<T> *elem, Node<T> *parent) {
+        if (elem->compareToNode(parent) <=
+            0) { //if element's value is less than or equal to parent's value, insert as left child
+            parent->setLeftChild(elem); //setting elem to be the child of its parent
+            return true; //inserted left child
+        } else if (elem->compareToNode(parent) > 0) { //if element's value is greater, insert as right child
+            parent->setRightChild(elem); //setting elem to be the child of its parent
+            return true; //inserted right child
+        }
+        return false; //insertion failed
+
+    }
 };
 
 int main() {
@@ -235,6 +324,7 @@ int main() {
     newBST->deleteElement(newData); // delete root
     newBST->print();
     newData = new Data(45);
+    //up to here works
     newBST->deleteElement(newData); //delete with two children
     newBST->print();
     newData = new Data(12);
